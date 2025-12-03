c23cd818a0ce7aa92aa3b9c0782f96ff1ea4a3f7
memawattushar-dot <memawattushar@gmail.com>
2025-12-03 06:51:36 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'tushara-agi-cache-v1';
+const urlsToCache = [
+    './index.html',
+    './manifest.json',
+    './tushara_logo_main.png' 
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
